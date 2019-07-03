05990f87f64040aac69f9cf19d64e3ef4959b017
Pourush Choudhary <31743206+atom1997@users.noreply.github.com>
2019-07-03 09:25:33 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import Header from "./header"
+import MemeGenerator from "./memeGenerator"
+
+function App() {
+    return(
+        <div>
+           <Header />
+           <MemeGenerator />
+        </div>
+    )
+}
+
+export default App
